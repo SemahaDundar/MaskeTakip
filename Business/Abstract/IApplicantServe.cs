29055f59f7a80bc4b24ef45548ccd0d7e76c02f2
Sema Dundar <d.semazeynep2022@gmail.com>
2024-02-11 14:37:12 +0300

    Maske Takip Tamamlandı

---
+++ Business/Abstract/IApplicantServe.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IApplicantServe
+    {
+        void ApplyForMask(Person person);
+
+
+        
+
+
+
+
+
+        List<Person> GetList();
+       
+
+        bool CheckPerson(Person person);
+     
+
+
+    }
+}
---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,39 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+
+    public class PttManager:ISupplierService
+    {
+        private IApplicantServe _applicantServe;
+
+
+
+
+        public PttManager(IApplicantServe applicantServe)// NEW YAPILDIĞINDA  CONTRUCTOR ÇALIŞIR
+        {
+            _applicantServe = applicantServe;
+
+
+        }
+        public void GiveMask(Person person)
+        {
+
+            if (_applicantServe.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + "için maske verildi ");
+            }
+            else
+            {
+                Console.WriteLine("Maske Verilemedi");
+            }
+        }
+
+    }
+}
